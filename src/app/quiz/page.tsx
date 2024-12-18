96e5e1670fdfc0542562500d3d84d07e4737020d
Sanjana Sree <139155698+Sanjanasree02@users.noreply.github.com>
2024-12-18 14:55:26 +0530

    Add files via upload

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,1 +1,25 @@
+import React from "react";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+import QuizCreation from "@/components/ui/QuizCreation";
 
+export const metadata = {
+  title: "Quiz | Quizzzy",
+  description: "Quiz yourself on anything!",
+};
+
+interface Props {
+  searchParams: {
+    topic?: string;
+  };
+}
+
+const Quiz = async ({ searchParams }: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    redirect("/");
+  }
+  return <QuizCreation topic={searchParams.topic ?? ""} />;
+};
+
+export default Quiz;
