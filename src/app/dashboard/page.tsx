6ff28538fc028017176da91a1fc6d1da85d1bc4e
Sanjana Sree <139155698+Sanjanasree02@users.noreply.github.com>
2024-12-18 14:49:49 +0530

    Add files via upload

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,1 +1,48 @@
+import HistoryCard from "@/components/dashboard/HistoryCard";
+import HotTopicsCard from "@/components/dashboard/HotTopicsCard";
+import QuizMeCard from "@/components/dashboard/QuizMeCard";
+import RecentActivityCard from "@/components/dashboard/RecentActivityCard";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+import React from "react";
 
+export const metadata = {
+  title: "Dashboard | Quizzzy",
+  description: "Quiz yourself on anything!",
+};
+
+// Mark this component explicitly as an async server component
+const Dashboard = async () => {
+  const session = await getAuthSession();
+  
+  // Redirect if the user is not authenticated
+  if (!session?.user) {
+    redirect("/");
+  }
+
+  return (
+    <main className="p-8 mx-auto max-w-7xl">
+      <div className="flex items-center">
+        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+      </div>
+
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizMeCard />
+        <HistoryCard />
+      </div>
+
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+        <HotTopicsCard />
+        <RecentActivityCard />
+      </div>
+
+      <div className="grid gap-4 mt-4 lg:grid-cols-2 w-full h-full">
+        <div className="lg:col-span-2 w-full h-[500px]">
+        
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Dashboard;
